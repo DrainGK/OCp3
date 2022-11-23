19cbb2bd012486bc717ab2a0495de32d37a70ce8
Guillaume Lopez <guillaumelopezgk@hotmail.com>
2022-11-23 22:35:35 +0900

    Version 5 form new project added

--- FrontEnd/form.js
+++ FrontEnd/form.js
@@ -1,45 +1,43 @@
-
 // Function to do ajax request post method using fetch
 function get_token(email, password) {
-    fetch('http://localhost:5678/api/users/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-            // Pour les requetes api qui ont besoin du token 'Authorization': 'Bearer ' + sessionStorage.getItem("token")
-        },
-        body: JSON.stringify({
-            'email': email,
-            'password': password
-        })
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      // Pour les requetes api qui ont besoin du token 'Authorization': 'Bearer ' + sessionStorage.getItem("token")
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  })
+    .then((response) => {
+      switch (response.status) {
+        case 200:
+          return response.json();
+        case 401:
+        case 404:
+          throw new Error("Invalid credentials");
+        default:
+          throw new Error("Something went wrong");
+      }
     })
-    .then(response => {
-        switch (response.status) {
-            case 200:
-                return response.json();
-            case 401:
-            case 404:
-                throw new Error('Invalid credentials');
-            default:
-                throw new Error('Something went wrong');
-        }
+    .then((data) => {
+      if (data.token !== undefined && data.token !== "") {
+        sessionStorage.setItem("token", data.token);
+        window.location.href = "index.html";
+      }
     })
-    .then(data => {
-        if (data.token !== undefined && data.token !== "") {
-            sessionStorage.setItem("token", data.token);
-            window.location.href = "/FrontEnd/index.html";
-        }
-    })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
 }
 
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-submit");
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const email = document.getElementById("E-mail").value;
-    const password = document.getElementById("password").value;
+  e.preventDefault();
+  const email = document.getElementById("E-mail").value;
+  const password = document.getElementById("password").value;
 
-    get_token(email, password);
-
+  get_token(email, password);
 });
--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -30,7 +30,7 @@ function galleryDisplay(projects) {
       (project) =>
         `
           <div class="photos-card">
-            <div id="binIcon" class="bin"> <i class="fa-regular fa-trash-can"></i> </div>
+            <div id="binIcon" class="bin" onclick="deleteProject(${project.id})"> <i class="fa-regular fa-trash-can"></i> </div>
             <img id="modale-photos" crossorigin="anonymous" src=${project.imageUrl} alt="photo ${project.title}">
             <a>éditer</a>
           </div>
@@ -40,25 +40,9 @@ function galleryDisplay(projects) {
   console.log(projects);
 }
 
-function projectDelete() {
-  fetch("http://localhost:5678/api/works", {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      // Pour les requetes api qui ont besoin du token 'Authorization': 'Bearer ' + sessionStorage.getItem("token")
-    },
-  }).then((response) => {
-    switch (response.status) {
-      case 200:
-        return response.json();
-      case 401:
-      case 500:
-        throw new Error("Unexpected Behaviour");
-      default:
-        throw new Error("Something went wrong");
-    }
-  });
-}
+const title = document.getElementById("add_title");
+const newProjectCategory = document.getElementById("category");
+
 const filterBtns = document.querySelectorAll(".btn");
 
 filterBtns.forEach(function (btn) {
@@ -84,9 +68,9 @@ function isConnected() {
 if (isConnected()) {
   document.getElementById("login").style.display = "none";
   document.getElementById("logout").style.display = "block";
-  document.getElementById("modify-bar").style.display = "block";
-  document.getElementById("modify-icon").style.display = "block";
-  document.getElementById("modify-icon-2").style.display = "block";
+  document.getElementById("modify-bar").style.display = "flex";
+  document.getElementById("modify-icon").style.display = "flex";
+  document.getElementById("modify-icon-2").style.display = "flex";
 } else {
   document.getElementById("login").style.display = "block";
   document.getElementById("logout").style.display = "none";
@@ -102,39 +86,109 @@ function logout() {
 const modifyGallery = document.getElementById("modify-icon-2");
 
 modifyGallery.addEventListener("click", (e) => {
-  document.querySelector(".modale").style.visibility = "visible";
-  document.querySelector(".modale-container").style.visibility = "visible";
-  document.querySelector(".modale-back").style.visibility = "visible";
+  document.querySelector(".modale").classList.toggle("hidden");
+  document.querySelector(".modale-container").classList.toggle("hidden");
+  document.querySelector(".modale-back").classList.toggle("hidden");
   e.preventDefault();
 });
 
 const closeModale = document.getElementById("cross-icon");
 
 closeModale.addEventListener("click", (e) => {
-  document.querySelector(".modale").style.visibility = "hidden";
-  document.querySelector(".modale-container").style.visibility = "hidden";
-  document.querySelector(".modale-back").style.visibility = "hidden";
+  document.querySelector(".modale").classList.toggle("hidden");
+  document.querySelector(".modale-container").classList.toggle("hidden");
+  document.querySelector(".modale-back").classList.toggle("hidden");
   e.preventDefault();
 });
 
 const addPhotos = document.getElementById("btn-photo");
 
 addPhotos.addEventListener("click", (e) => {
-  document.querySelector(".modale-container").style.visibility = "hidden";
-  document.querySelector(".add-photo").style.visibility = "visible";
+  document.querySelector(".modale-container").classList.toggle("hidden");
+  document.querySelector(".add-photo").classList.toggle("hidden");
   e.preventDefault();
 });
 const previousModale = document.getElementById("left-arrow");
 
 previousModale.addEventListener("click", (e) => {
-  document.querySelector(".add-photo").style.visibility = "hidden";
-  document.querySelector(".modale-container").style.visibility = "visible";
+  document.querySelector(".add-photo").classList.toggle("hidden");
+  document.querySelector(".modale-container").classList.toggle("hidden");
   e.preventDefault();
 });
 
-const deleteProject = document.getElementById("binIcon");
+function deleteProject(id) {
+  deleteProject(id);
+}
 
-deleteProject.addEventListener("click", (e) => {
-  console.log("clicked");
-  e.preventDefault();
+async function deleteProject(id) {
+  let token = sessionStorage.getItem("token");
+
+  await fetch(`http://localhost:5678/api/works/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => (projectsData = data));
+  projectsDisplay(projectsData);
+  galleryDisplay(projectsData);
+}
+
+async function postProject() {
+  let token = sessionStorage.getItem("token");
+
+  await fetch("http://localhost:5678/api/works", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+      // Pour les requetes api qui ont besoin du token 'Authorization': 'Bearer ' + sessionStorage.getItem("token")
+    },
+    body: JSON.stringify({
+      categoryId: newProjectCategory.value,
+      title: title.value,
+      imageUrl: `url(${uploaded_image})`,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => (projectsData = data));
+  projectsDisplay(projectsData);
+  galleryDisplay(projectsData);
+}
+
+const image_input = document.querySelector("#image_input");
+var uploaded_image = "";
+
+image_input.addEventListener("change", function () {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    uploaded_image = reader.result;
+    document.querySelector(
+      "#display_image"
+    ).style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+
+  if ((uploaded_image = true)) {
+    document.getElementById("image-icon").style.display = "none";
+    document.getElementById("display_image").style.display = "block";
+  } else {
+    document.getElementById("image-icon").style.display = "block";
+    document.getElementById("display_image").style.display = "none";
+  }
+});
+
+const addPhotosBtn = document.getElementById("add-photo-btn");
+
+addPhotosBtn.addEventListener("click", (e) => {
+  const clickPhotoBtn = document.getElementById("image_input");
+  clickPhotoBtn.click();
+});
+
+const newProjectBtn = document.getElementById("submit-photo-id");
+
+newProjectBtn.addEventListener("click", (e) => {
+  e.preventDefault;
 });
